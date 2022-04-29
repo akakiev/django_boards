c3be10f4bbae8e125b2e7c60ba7693e03383bf12
ILIM\SBaraban <baraban.s.v@vntu.edu.ua>
2022-04-29 15:34:41 +0200

    It is a discussion board(a forum).

---
+++ README.rst
@@ -0,0 +1,24 @@
+=============
+Django Boards
+=============
+
+This project is a discussion board (a forum). The whole idea is to maintain several boards, which will behave like categories. Then, inside a specific board, a user can start a new discussion by creating a new topic. In this topic, other users can engage in the discussion posting replies.
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+
+1. Clone the repository to your local machine::
+
+    git clone git@github.com:sibtc/django-beginners-guide.git
+
+2. Create the database:
+
+    python manage.py migrate
+
+3. Finally, run the development server:
+
+    python manage.py runserver
+
+4. Start the development server and visit http://127.0.0.1:8000/
---
+++ myproject/urls.py
@@ -0,0 +1,51 @@
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from boards import views
+from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('boards/<pk>/', views.board_topics, name='board_topics'),
+    path('boards/<pk>/new/', views.new_topic, name='new_topic'),
+
+    path('reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+    ), name='password_reset'),
+
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+    name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+    name='password_reset_confirm'),
+
+    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+    name='password_reset_complete'),
+
+    path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),
+
+]
